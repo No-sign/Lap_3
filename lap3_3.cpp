552e5889fc3d4b75492c021fdeb84821724ed018
No-sign <55203083+No-sign@users.noreply.github.com>
2019-12-27 15:43:53 +0700

    Add files via upload

---
+++ lap3_3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+float x=2,sum;
+int main(){
+   //ios::sync_with
+    
+    while(x<=69){
+    sum=sum+(1/x);
+    x++;
+}
+
+   cout<<sum<<endl;
+    
+    
+    
+   
+    
+    
+    return 0;
+}
+
+    
